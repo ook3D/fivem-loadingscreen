c5ad33806a74258a3f8869ef38f6a786eec35aea
ooknumber13 <47336113+ooknumber13@users.noreply.github.com>
2023-04-18 06:25:09 -0400

    Add files via upload

---
+++ loadscreen/fxmanifest.lua
@@ -0,0 +1,17 @@
+--[[ FX Information ]]--
+fx_version   "cerulean"
+game         "gta5"
+
+
+--[[ Resource Information ]]--
+name         "loadscreen"
+author       "ook_3d"
+version      "1.0.0"
+description  "Loading Screen For FiveM"
+
+
+files {
+    'index.html'
+}
+
+loadscreen 'index.html'
